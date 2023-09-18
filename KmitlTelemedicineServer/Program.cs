351c53e69c1d227e3fc8244a53514d43ab536543
sthairno <45668272+sthairno@users.noreply.github.com>
2023-09-18 18:20:23 +0700

    update

--- KmitlTelemedicineServer/Program.cs
+++ KmitlTelemedicineServer/Program.cs
@@ -1,31 +1,27 @@
-using System.Security.Claims;
 using AspNetCore.Firebase.Authentication.Extensions;
 using FirebaseAdmin;
-using FirebaseAdmin.Auth;
 using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Firestore;
 using KmitlTelemedicineServer;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 internal class Program
 {
     private static string _projectId;
-    
+
     public static void Main(string[] args)
     {
         FirebaseApp.Create();
-        _projectId = 
-            (FirebaseApp.DefaultInstance.Options.Credential.UnderlyingCredential 
+        _projectId =
+            (FirebaseApp.DefaultInstance.Options.Credential.UnderlyingCredential
                 as ServiceAccountCredential)!
             .ProjectId;
 
         WebApplication app = BuildApplication(args);
-        
+
         Configure(app);
 
         app.Run();
@@ -34,29 +30,29 @@ internal class Program
     static WebApplication BuildApplication(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
-        
+
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.Configure<ForwardedHeadersOptions>(options =>
         {
             options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
         });
         builder.Services.AddServerConfig(builder.Configuration);
-        builder.Services.AddSingleton<FirestoreDb>(provider => FirestoreDb.Create(_projectId));
+        builder.Services.AddSingleton<FirestoreDb>(_ => FirestoreDb.Create(_projectId));
         builder.Services.AddFirebaseAuthentication(_projectId);
         builder.Services.AddAuthorization();
         builder.Services.AddSwaggerGen(options =>
         {
             var securityScheme = new OpenApiSecurityScheme
             {
+                Name = "FirebaseJwtBarer",
                 Type = SecuritySchemeType.Http,
-                Scheme = JwtBearerDefaults.AuthenticationScheme,
-                BearerFormat = "JWT"
+                Scheme = JwtBearerDefaults.AuthenticationScheme
             };
-            
-            options.AddSecurityDefinition("JwtBarer", securityScheme);
+
+            options.AddSecurityDefinition(securityScheme.Name, securityScheme);
             options.AddSecurityRequirement(new OpenApiSecurityRequirement
             {
-                {securityScheme, Array.Empty<string>()}
+                { securityScheme, Array.Empty<string>() }
             });
         });
         builder.Services.AddHttpContextAccessor();
@@ -69,24 +65,24 @@ internal class Program
                 builder.AllowAnyMethod();
             });
         });
-        
+
         return builder.Build();
     }
 
     static void Configure(WebApplication app)
     {
         var config = app.Services.GetRequiredService<IOptions<ServerConfig>>().Value;
-        
+
         if (!string.IsNullOrWhiteSpace(config.PathBase))
         {
             app.Logger.LogInformation("Path base: {PathBase}", config.PathBase);
             app.UsePathBase(config.PathBase);
             app.UseRouting();
         }
-        
+
         app.UseAuthentication();
         app.UseAuthorization();
-        
+
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
@@ -95,7 +91,7 @@ internal class Program
         }
 
         app.UseForwardedHeaders();
-        
+
         app.MapApiEndpoints();
     }
 }
